fb63fc50e3a7fd0d43cdcb7cf845c5ba99262f42
Rizal Fadlullah <92254049+izzalDev@users.noreply.github.com>
2024-03-25 12:40:56 +0000

    pertemuan 3

--- src/MobilBeraksi.java
+++ src/MobilBeraksi.java
@@ -1,6 +1,10 @@
 class MobilBeraksi {
     public static void main(String[] args) {
         Mobil mbl = new Mobil();
+        Mobil mobil2 = new Mobil("SUV", "FORTUNER", 2022);
+        Mobil mobil3 = new Mobil("MPV", "HONDA JAZZ", 2019);
+        Mobil mobil4 = new Mobil("MPV", "TOYOTA AVANZA", 2023);
+
         mbl.setJenis("MPV");
         mbl.setNama("TOYOTA AVANZA");
         mbl.setTahun(2017);
@@ -8,5 +12,10 @@ class MobilBeraksi {
         System.out.println(mbl.getNama());
         System.out.println(mbl.getJenis());
         System.out.println(mbl.getTahun());
+        System.out.println(mobil2.getNama());
+
+        mobil2.detilMobil();
+        mobil3.detilMobil();
+        mobil4.detilMobil();
     }
 }
---
+++ src/SmartphoneBeraksi.java
@@ -0,0 +1,6 @@
+public class SmartphoneBeraksi {
+    public static void main(String[] args) {
+        Smartphone smartphone1 = new Smartphone("Samsung", 2500000, 2015);
+        smartphone1.showSmartphone();
+    }
+}
